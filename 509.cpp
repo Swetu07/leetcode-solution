98bd4f9e5a776cdad794902ddf586af5831a86a4
Swetank Kumar <swetankkumar15f@gmail.com>
2026-07-30 18:09:00 +0530

    509 solution

---
+++ 509.cpp
@@ -0,0 +1,70 @@
+// 509 solution
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter n: ";
+    cin >> n;
+
+    if (n == 0)
+    {
+        cout << 0;
+        return 0;
+    }
+
+    if (n == 1)
+    {
+        cout << 1;
+        return 0;
+    }
+
+    int prev2 = 0;
+    int prev1 = 1;
+    int current;
+
+    for (int i = 2; i <= n; i++)
+    {
+        current = prev1 + prev2;
+
+        prev2 = prev1;
+
+        prev1 = current;
+    }
+
+    cout << current;
+
+    return 0;
+}
+
+// leetcode solution 
+/*
+class Solution
+{
+public:
+    int fib(int n)
+    {
+        if (n == 0)
+            return 0;
+
+        if (n == 1)
+            return 1;
+
+        int prev2 = 0;
+        int prev1 = 1;
+        int current;
+
+        for (int i = 2; i <= n; i++)
+        {
+            current = prev1 + prev2;
+
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return current;
+    }
+};
+*/
