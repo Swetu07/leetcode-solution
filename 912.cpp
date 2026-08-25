46a106ff54fadbb30ae8887ccb4edd7d97b19212
Swetank Kumar <swetankkumar15f@gmail.com>
2026-08-25 22:32:22 +0530

    912 using bubble sort

--- 912.cpp
+++ 912.cpp
@@ -1,57 +1,57 @@
 // 912 solution Selection Sort
-#include <iostream>
-#include <vector>
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// using namespace std;
 
-vector<int> sortArray(vector<int>& nums)
-{
-    int n = nums.size();
+// vector<int> sortArray(vector<int>& nums)
+// {
+//     int n = nums.size();
 
-    for (int i = 0; i < n - 1; i++)
-    {
-        int minIndex = i;
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         int minIndex = i;
 
-        for (int j = i + 1; j < n; j++)
-        {
-            if (nums[j] < nums[minIndex])
-            {
-                minIndex = j;
-            }
-        }
+//         for (int j = i + 1; j < n; j++)
+//         {
+//             if (nums[j] < nums[minIndex])
+//             {
+//                 minIndex = j;
+//             }
+//         }
 
-        swap(nums[i], nums[minIndex]);
-    }
+//         swap(nums[i], nums[minIndex]);
+//     }
 
-    return nums;
-}
+//     return nums;
+// }
 
-int main()
-{
-    int n;
+// int main()
+// {
+//     int n;
 
-    cout << "Enter size: ";
-    cin >> n;
+//     cout << "Enter size: ";
+//     cin >> n;
 
-    vector<int> nums(n);
+//     vector<int> nums(n);
 
-    cout << "Enter elements: ";
+//     cout << "Enter elements: ";
 
-    for (int i = 0; i < n; i++)
-    {
-        cin >> nums[i];
-    }
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> nums[i];
+//     }
 
-    nums = sortArray(nums);
+//     nums = sortArray(nums);
 
-    cout << "Sorted array: ";
+//     cout << "Sorted array: ";
 
-    for (int x : nums)
-    {
-        cout << x << " ";
-    }
+//     for (int x : nums)
+//     {
+//         cout << x << " ";
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
 
 /*
 Leetcode Solution (but its not allowed using Selection Sort)
@@ -74,6 +74,90 @@ public:
             }
 
             swap(nums[i], nums[minIndex]);
+        }
+
+        return nums;
+    }
+};
+*/
+
+// 912 Solution using Bubble Sort
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> sortArray(vector<int>& nums)
+{
+    int n = nums.size();
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        bool swapped = false;
+
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (nums[j] > nums[j + 1])
+            {
+                swap(nums[j], nums[j + 1]);
+                swapped = true;
+            }
+        }
+
+        if (swapped == false)
+        {
+            break;
+        }
+    }
+
+    return nums;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+    nums = sortArray(nums);
+
+    for (int x : nums)
+    {
+        cout << x << " ";
+    }
+
+    return 0;
+}
+
+// Leetcode Solution (bubble)
+/*
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums)
+    {
+        int n = nums.size();
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            bool swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (nums[j] > nums[j + 1])
+                {
+                    swap(nums[j], nums[j + 1]);
+                    swapped = true;
+                }
+            }
+
+            if (!swapped)
+            {
+                break;
+            }
         }
 
         return nums;
