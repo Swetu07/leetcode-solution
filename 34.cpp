75968ece113d128f06c6fab6f9f533647f04a373
Swetank Kumar <swetankkumar15f@gmail.com>
2026-09-10 09:56:17 +0530

    34 solution

---
+++ 34.cpp
@@ -0,0 +1,89 @@
+// 34 solution
+#include <bits/stdc++.h>
+using namespace std;
+
+int firstPosition(vector<int>& nums, int target)
+{
+    int low = 0;
+    int high = nums.size() - 1;
+    int ans = -1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else if (nums[mid] < target)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    return ans;
+}
+
+int lastPosition(vector<int>& nums, int target)
+{
+    int low = 0;
+    int high = nums.size() - 1;
+    int ans = -1;
+
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target)
+        {
+            ans = mid;
+            low = mid + 1;
+        }
+        else if (nums[mid] < target)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    int n;
+
+    cout << "Enter N: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter sorted array: ";
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+    int target;
+
+    cout << "Enter target: ";
+    cin >> target;
+
+    int first = firstPosition(nums, target);
+    int last = lastPosition(nums, target);
+
+    cout << "First position: " << first << endl;
+    cout << "Last position: " << last << endl;
+
+    return 0;
+}
